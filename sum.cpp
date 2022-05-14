fb5f6d1f36edd668b2da0f526b24367a0a3505b2
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2022-05-14 22:08:22 +0530

    buouob

--- sum.cpp
+++ sum.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
-    
+    int amt1;
+    cout<<"Enter amount 1: ";
+    cin>>amt1;
+    cout<<"Enter amount 2: ";
+    int amt2;
+    cin>>amt2;
+    int sum = amt1+amt2;
+    cout<<"The total amount is "<<sum<<endl;
     return 0;
 }
